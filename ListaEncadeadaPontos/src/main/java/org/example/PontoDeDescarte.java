c5697f7f58aa9d69caffaba13c5aa79b0d2c37f3
BrunoAbrahim <145516226+BrunoAbrahim@users.noreply.github.com>
2025-04-11 16:29:10 -0300

    PontoDeDescarte.java

--- ListaEncadeadaPontos/src/main/java/org/example/PontoDeDescarte.java
+++ ListaEncadeadaPontos/src/main/java/org/example/PontoDeDescarte.java
@@ -6,7 +6,6 @@ public class PontoDeDescarte {
     String endereco;
     PontoDeDescarte proximo;
 
-    // Construtor para inicializar os dados do ponto
     public PontoDeDescarte(int id, String tipoMaterial, String endereco) {
         this.id = id;
         this.tipoMaterial = tipoMaterial;
